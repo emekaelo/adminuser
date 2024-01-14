05b7082cc2770c11f09fc83184865ec28642e1d6
emekaelo <devemekaelo@gmail.com>
2024-01-14 11:10:53 +0100

    chore: add supabase js client dependency

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://hdpavvkkfcaiyhgehfld.supabase.co',
+  /**
+   * supabase `anon` key
+   */
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcGF2dmtrZmNhaXloZ2VoZmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUxMjQxODksImV4cCI6MjAyMDcwMDE4OX0.eSvGT4nWq-drmwN8mcRCUQ-hX-jF2buA1JFwnssk5FE',
+  /**
+   * supabase `service_role` key
+   */
+  supabaseServiceRoleKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcGF2dmtrZmNhaXloZ2VoZmxkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNTEyNDE4OSwiZXhwIjoyMDIwNzAwMTg5fQ.t2pga5UE6Z-h-6KKz5UMIPyGDOfJp6UCNM9Z8HhK030',
+}
